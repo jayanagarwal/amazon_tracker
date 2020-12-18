ed0926060d8d3021e8c8172194f1d4fc88395d59
Jayan Agarwal <agarwaljayan@gmail.com>
2020-12-18 11:59:53 +0530

    Basic Extracting

--- amazon_tracker.py
+++ amazon_tracker.py
@@ -13,3 +13,25 @@ prod_tracker_URLS = prod_tracker.url
 page = requests.get(prod_tracker_URLS[0], headers = HEADERS)
 
 soup = BeautifulSoup(page.content, features="lxml")
+
+#product title
+title = soup.find(id='productTitle').get_text().strip()
+
+try:
+    price = float(soup.find(id="priceblock_ourprice").get_text().replace('₹','').replace(',','').strip())
+except:
+    price =""
+
+review_score = float(soup.select('.a-star-4-5')[0].get_text().split()[0].replace(',','.'))
+
+#print(price,"\t",review_score)
+
+review_count = int(soup.select('#acrCustomerReviewText')[0].get_text().split()[0])
+# print(review_count)
+
+try:
+    soup.select('#availability .a-color-state')[0].get_text().strip()
+    stock = 'Out of Stock'
+except:
+    stock = "Available"
+
